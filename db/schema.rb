dac949a2629cca46881aeddfc8e7162b7f0f2899
Maxime Trinquet <maxime.trinquet@gmail.com>
2020-03-11 14:46:29 +0800

    push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_11_033528) do
+ActiveRecord::Schema.define(version: 2020_03_11_064401) do
+
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.text "description"
+    t.integer "price_per_night"
+    t.integer "number_of_guests"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
