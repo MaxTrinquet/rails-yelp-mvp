c666b43710818932d2d72cccfced9d90b92c4b1f
Maxime Trinquet <maxime.trinquet@gmail.com>
2020-03-11 13:34:07 +0800

    Restaurants finised

---
+++ db/migrate/20200311033528_adressto_address.rb
@@ -0,0 +1,5 @@
+class AdresstoAddress < ActiveRecord::Migration[5.2]
+  def change
+    rename_column :restaurants, :adress, :address
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts 'Destroy database'
+Restaurant.destroy_all
+
+Restaurant.create(name: Faker::Restaurant.name,
+                  address: Faker::Address.full_address,
+                  category: 'belgium',
+                  phone_number: '+65 7345 5388'
+                  )
+Restaurant.create(name: Faker::Restaurant.name,
+                  address: Faker::Address.full_address,
+                  category: 'japanese',
+                  phone_number: '+65 7344 5388'
+                  )
+Restaurant.create(name: Faker::Restaurant.name,
+                  address: Faker::Address.full_address,
+                  category: 'japanese',
+                  phone_number: '+65 4345 5388'
+                  )
+Restaurant.create(name: Faker::Restaurant.name,
+                  address: Faker::Address.full_address,
+                  category: 'chinese',
+                  phone_number: '+65 7345 7676'
+                  )
+Restaurant.create(name: Faker::Restaurant.name,
+                  address: Faker::Address.full_address,
+                  category: 'belgium',
+                  phone_number: '+65 7345 9999'
+                  )
+Restaurant.create(name: Faker::Restaurant.name,
+                  address: Faker::Address.full_address,
+                  category: 'italian',
+                  phone_number: '+65 7345 7878'
+                  )
+Restaurant.create(name: Faker::Restaurant.name,
+                  address: Faker::Address.full_address,
+                  category: 'french',
+                  phone_number: '+65 7345 6767')
+
+puts 'database created !'
